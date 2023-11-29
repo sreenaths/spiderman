14b9d8aea32ac9e6767068fd1bbaff813f00ff7c
sreenaths <somarajapuram@gmail.com>
2023-11-29 08:46:28 -0800

    Print stats

--- scripts/build_dataset.py
+++ scripts/build_dataset.py
@@ -1,32 +1,45 @@
+from os import path
+from yaspin import yaspin
+
 from libs.source import SourceReader, File
 from libs.db import Database
 from libs.sink import write_csv, delete_dir
-from os import path
+from libs.utils import print_stats
 
 SINK_PATH = '../dataset'
-def process_db(db_name: str, data: bytes):
-    print(f"Processing file: {db_name}")
+def process_db(db_name: str, data: bytes) -> list[str]:
+    processed_tables: list[str] = []
 
     with Database(data) as db:
         table_names = db.list_tables()
 
         for table_name in table_names:
-            print(f"Writing data for {table_name}...")
             table_data = db.read_table(table_name)
 
-            sync_file_path = path.join(SINK_PATH, db_name, f'{table_name}.csv')
-            write_csv(sync_file_path, table_data)
-            print("Done")
+            data_is_missing = len(table_data) > 1
+            if data_is_missing:
+                sync_file_path = path.join(SINK_PATH, db_name, f'{table_name}.csv')
+                write_csv(sync_file_path, table_data)
+                processed_tables.append(table_name)
+
+    return processed_tables
 
 
 SOURCE_PATH = './source/spider.zip'
-def process_source():
+def process_source() -> list:
+    dbs = []
+
     with SourceReader(SOURCE_PATH) as source:
         files = source.list_files()
-        print('Files found: ', len(files))
 
         for file in files:
-            process_db(file.name, source.read_file(file))
+            tables = process_db(file.name, source.read_file(file))
+            dbs.append((file.name, tables))
+
+    return dbs
+
 
 delete_dir(SINK_PATH)
-process_source()
+with yaspin(text="Processing..."):
+    dbs = process_source()
+print_stats(dbs)
